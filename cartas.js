1b87080761d9d95afe1ca706b37d1dcd754e0600
João Lucas Sampaio <160886470+joaolucassampaio@users.noreply.github.com>
2024-04-04 15:50:11 -0300

    Update cartas.js

--- cartas.js
+++ cartas.js
@@ -1,3 +1,10 @@
+//Escreva um programa em javascript para simular um baralho de cartas. 
+//O programa deve iniciar mostrando na tela um menu interativo contendo a quantidade de cartas que estão atualmente no baralho e as opções de “Adicionar uma carta”,
+//“Puxar uma carta” e “Sair”. 
+//Ao escolher “Adicionar uma carta”, o programa deve perguntar o nome da carta e adicioná-la no topo do baralho. 
+//Ao escolher “Puxar uma carta”, o programa deve retirar a carta do topo do baralho e mostrar na tela o nome da carta puxada. 
+//O programa só deve ser encerrado ao escolher a opção de “Sair”, caso contrário deve voltar ao menu.
+
 alert("Pilha de Cartas!")
 
 const array = ["Ás de Paus", "Dama de Copas", "Rei de Ouros", "Valete de Espadas", "Dois de Copas", "Quatro de Paus", "Seis de Ouros", "Oito de Espadas", "Três de Copas", "Cinco de Paus", "Sete de Ouros", "Nove de Espadas", "Ás de Espadas", "Dez de Copas", "Rei de Paus", "Nove de Ouros", "Valete de Copas", "Três de Espadas", "Seis de Espadas", "Dois de Espadas"]
